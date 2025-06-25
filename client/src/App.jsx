7d22cc1d79a5c8b0b9e4c2589607a91e4e5946de
Mahima Dewangan <mahimadewangan11@gmail.com>
2025-06-25 14:22:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react'
+import {  Routes , Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import  'react-toastify/dist/ReactToastify.css';
+
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
+import Navbar from  './components/Navbar'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import { AppContext } from './context/AppContext'
+
+const App = () => {
+
+  const {showLogin} = useContext(AppContext)
+  return (
+    
+
+      <div className='px-4 sm:px-10 md:px-14 lg:px-28
+      min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+
+        <ToastContainer position='botttom-right'/>
+
+        {/* imported NAVBAR in the app file because navbar is supposed to be visible on all the pages */}
+        <Navbar/>
+        { showLogin && <Login/>}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      
+        <Route path='/result' element={<Result/>}/>
+      
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
+
+      {/* imported footer in the app file as footer is supposed to be visible on all the pages  */}
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default App
